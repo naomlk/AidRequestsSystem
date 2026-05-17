3b2f1fce4d503dd2d36a60dac119f3018ac3217e
perez <perezclaire868@gmail.com>
2026-05-17 12:18:22 +0300

    add some comments

--- StageC/integration.py
+++ StageC/integration.py
@@ -27,11 +27,11 @@ output_file = Path("backup_groupA_clean.sql")
 
 sql = input_file.read_text(encoding="utf-16")
 
-
+#ETAPE 1
 #REMPLACER TTES LES TABLES table->b_table  d
 
 for table in tablesGroupA:   #changer ttes les tables
-    sql = sql.replace(f"{table}", f"a_{table}")
+    sql = sql.replace(f"public.{table}", f"a_{table}")  #changer QUE les tables
 # mais attention ca m efait des erreur car suposons qu ejai une colonne qui sapple delivery_id alors elle deviendra a_delivery_id
 # et on ne doit pas touche aux colonnes des tables donc il faut ecrire el scritp au cas par acs en focntion
 # de la maniere dont le backuop ets ecrit
@@ -39,6 +39,9 @@ for table in tablesGroupA:   #changer ttes les tables
 sql = sql.replace("CREATE SCHEMA public;", "-- CREATE SCHEMA public;")    # pas compris pourquoi et cest quoi
 sql = sql.replace("ALTER SCHEMA public OWNER TO pg_database_owner;", "-- ALTER SCHEMA public OWNER TO pg_database_owner;")
 
+
+#ETAPE 2-- INDEX ?
+#ETAPE 3-- constraints ?
 output_file.write_text(sql, encoding="utf-8")   # ici il faut changer en focntion de la version du backup file
 
 print("Backup modifié créé :", output_file)
